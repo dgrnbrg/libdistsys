b98d5f6144b3ddbcab06e06dfce27414233a4ff4
David Greenberg <dsg123456789@gmail.com>
2014-11-04 21:49:24 -0500

    Add CRDT and introduce dot abstraction

--- src/distrlib/orswot.clj
+++ src/distrlib/orswot.clj
@@ -1,15 +1,29 @@
 (ns distrlib.orswot
-  (:refer-clojure :exclude (get assoc dissoc merge))
+  (:refer-clojure :exclude (get assoc dissoc merge resolve))
   (:require [clojure.test.check :as tc]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clojure.set :as set]))
 
+(defprotocol CRDT
+  (resolve* [r1 r2] "Merges 2 instances of a CRDT")
+  ;;TODO how to handle shared version vectors in composites?
+  )
+
+(defn resolve
+  "Given CRDTs, it combines them."
+  ([] nil)
+  ([crdt1] crdt1)
+  ([crdt1 crdt2] (resolve* crdt1 crdt2))
+  ([crdt1 crdt2 & more]
+   (reduce resolve (resolve crdt1 crdt2) more)))
+
 ;; local version are ints
 
-;; This is the value of a key + the node & version it was stored at
-;; node & local version form the dot
-(defrecord Value [node local-version value])
+;; A dot is a node & a local time for that node.
+;; version vectors are collections of dots. some
+;; optimized CRDTs, like orswot & dvv use them as well
+(defrecord Dot [node time])
 
 ;; version is a map from node ID to local version
 ;; data is a map from keys to Value pairs
@@ -27,67 +41,60 @@
 
 (defn assoc
   "Takes an orswot, a node, a local-version, and list of key value pairs"
-  ([orswot node local-version k]
-   (assoc orswot node local-version k nil))
-  ([orswot node local-version k v]
+  ([orswot dot k]
 ;;     (assert (< (get-in orswot [:version node] -1) local-version))
-   (let [version' (clojure.core/assoc (:version orswot) node local-version)
-         data' (clojure.core/assoc (:data orswot) k (->Value node local-version v))]
+   (let [version' (clojure.core/assoc (:version orswot) (:node dot) (:time dot))
+         data' (clojure.core/assoc (:data orswot) k dot)]
      (->Orswot version' data')))
-  ([orswot node local-version k v & kvs]
-   ;(assoc orswot node local-version k v)
-   )
-  )
+  ([orswot dot k & more]
+   (reduce #(assoc %1 dot) (assoc orswot dot k) more)))
 
 (defn dissoc
-  [orswot node local-version k]
+  [orswot dot k]
   ;; TODO: make these asserts validate at a higher level
  ;; (assert (< (get-in orswot [:version node] -1) local-version))
-  (let [version' (clojure.core/assoc (:version orswot) node local-version)
+  (let [version' (clojure.core/assoc (:version orswot) (:node dot) (:time dot))
         data' (clojure.core/dissoc (:data orswot) k)]
     (->Orswot version' data')))
 
-(defn merge
-  ([] (orswot))
-  ([orswot] orswot)
-  ([orswot1 orswot2]
-   (let [keys-in-left (:data orswot1)
-         keys-in-right (:data orswot2)
-         only-in-left (remove (partial contains? keys-in-right) (keys keys-in-left))
-         only-in-right (remove (partial contains? keys-in-left) (keys keys-in-right))
-         kept-left (filter (fn [k]
-                             (let [{dot-node :node dot-version :local-version} (clojure.core/get keys-in-left k)]
-                               (> dot-version (get-in orswot2 [:version dot-node] -1))))
-                           only-in-left)
-         kept-right (filter (fn [k]
-                              (let [{dot-node :node dot-version :local-version} (clojure.core/get keys-in-right k)]
-                                (> dot-version (get-in orswot1 [:version dot-node] -1))))
-                            only-in-right)
-         keys-in-both (filter (partial contains? keys-in-right) (keys keys-in-left))
-         merged-version (merge-with max (:version orswot1 -1) (:version orswot2 -1))
-         ;;TODO handle value conflicts
-         merged-both (clojure.core/merge (select-keys keys-in-left (concat kept-left keys-in-both))
-                                         (select-keys keys-in-right kept-right))]
-     (->Orswot merged-version merged-both)))
-  ([o1 o2 & os]
-   (reduce merge (list* o1 o2 os))))
+(extend-type Orswot
+  CRDT
+  (resolve* [orswot1 orswot2]
+    (let [keys-in-left (:data orswot1)
+          keys-in-right (:data orswot2)
+          only-in-left (remove (partial contains? keys-in-right) (keys keys-in-left))
+          only-in-right (remove (partial contains? keys-in-left) (keys keys-in-right))
+          kept-left (filter (fn [k]
+                              (let [{:keys [node time]} (clojure.core/get keys-in-left k)]
+                                (> time (get-in orswot2 [:version node] -1))))
+                            only-in-left)
+          kept-right (filter (fn [k]
+                               (let [{:keys [node time]} (clojure.core/get keys-in-right k)]
+                                 (> time (get-in orswot1 [:version node] -1))))
+                             only-in-right)
+          keys-in-both (filter (partial contains? keys-in-right) (keys keys-in-left))
+          merged-version (merge-with max (:version orswot1 -1) (:version orswot2 -1))
+          ;;TODO handle value conflicts
+          merged-both (clojure.core/merge (select-keys keys-in-left (concat kept-left keys-in-both))
+                                          (select-keys keys-in-right kept-right))]
+      (->Orswot merged-version merged-both))))
 
 (def x1 (-> (orswot)
-            (assoc :n1 0 "hello")))
+            (assoc (->Dot :n1 0) "hello")))
 
 (def x2 (-> (orswot)
-            (assoc :n3 0 "привет")
-            (assoc :n2 1 "hi")
+            (assoc (->Dot :n3 0) "привет")
+            (assoc (->Dot :n2 1) "hi")
             ))
 
 (def x3
   (-> (orswot)
-      (assoc :n2 1 "hi")
-      (dissoc :n2 2 "hi")
+      (assoc (->Dot :n2 1) "hi")
+      (dissoc (->Dot :n2 2) "hi")
       ))
 
-(keys (:data (merge x1 x2)))
-(keys (:data (merge x1 x2 x3)))
+(keys (:data (resolve x1 x2)))
+(keys (:data (resolve x1 x2 x3)))
 
 (defn keyset-of-orswot
   [orswot]
@@ -98,7 +105,7 @@
                 (= (set v)
                    (keyset-of-orswot
                      (reduce (fn [orswot [k i]]
-                               (assoc orswot :node i k))
+                               (assoc orswot (->Dot :node i) k))
                              (orswot)
                              (map vector v (range)))))))
 
@@ -116,28 +123,30 @@
   [node ops]
   (reduce (fn [orswot {:keys [k op i]}]
                                (case op
-                                 :add (assoc orswot node i k)
-                                 :remove (dissoc orswot node i k)
+                                 :add (assoc orswot (->Dot node i) k)
+                                 :remove (dissoc orswot (->Dot node i) k)
                                  :noop orswot))
                              (orswot)
                              (map #(clojure.core/assoc %1 :i %2) ops (range))))
 
-(def set-semantics
-  (prop/for-all [ops (gen/vector (gen/hash-map :k gen/int
-                                               :op (gen/elements [:add :remove])) 10 1000)]
-                (= (run-ops-as-set ops)
-                   (keyset-of-orswot
-                     (run-ops-as-orswot :node ops)))))
-
 (def gen-biased-op (gen/frequency [[5 (gen/return :add)] [1 (gen/return :remove)]]))
+
+(def gen-key (gen/elements (map #(keyword (str "key" %)) (range 50))))
 
 (def the-nodes [:n1 :n2 :n3 :n4 :n5])
 
 (def gen-node (gen/elements the-nodes))
 
+(def set-semantics
+  (prop/for-all [ops (gen/vector (gen/hash-map :k gen-key
+                                               :op gen-biased-op) 10 1000)]
+                (= (run-ops-as-set ops)
+                   (keyset-of-orswot
+                     (run-ops-as-orswot :node ops)))))
+
 (def merge-of-peers
   (prop/for-all [ops (gen/vector (gen/hash-map :node gen-node
-                                               :k gen/int
+                                               :k gen-key
                                                :op gen-biased-op) 10 1000)]
                 (= (->> (group-by :node ops)
                         vals
@@ -146,7 +155,7 @@
                    (keyset-of-orswot
                      (->> (group-by :node ops)
                           (map (fn [[node ops]] (run-ops-as-orswot node ops)))
-                          (apply merge))))))
+                          (apply resolve))))))
 
 (defn prefixes
   [s]
@@ -154,7 +163,7 @@
 
 (def merge-commutative
   (prop/for-all [ops (gen/vector (gen/hash-map :node gen-node
-                                               :k gen/int
+                                               :k gen-key
                                                :op gen-biased-op) 10 1000)
                  knuth-suffle (apply gen/tuple (map gen/elements (prefixes (range (count the-nodes)))))]
                 (let [orswots (->> (concat (map #(hash-map :op :noop :node %) the-nodes)
@@ -167,13 +176,14 @@
                                                              j (nth perm i)))
                                        (vec orswots)
                                        (map vector (range) knuth-suffle))]
-                  (= (keyset-of-orswot (apply merge orswots))
-                     (keyset-of-orswot (apply merge permuted))))))
+                  (= (keyset-of-orswot (apply resolve orswots))
+                     (keyset-of-orswot (apply resolve permuted))))))
 
 (comment
-  (tc/quick-check 100 keys-added-remain)
-  (tc/quick-check 100 set-semantics)
-  (tc/quick-check 100 merge-of-peers)
-  (tc/quick-check 100 merge-commutative)
+  (do
+    (tc/quick-check 100 keys-added-remain-one-node)
+    (tc/quick-check 100 set-semantics)
+    (tc/quick-check 100 merge-of-peers)
+    (tc/quick-check 100 merge-commutative))
   )
 
