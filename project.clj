972a482568d450043a3825446ea20e6c0ac30e41
David Greenberg <dsg123456789@gmail.com>
2014-11-11 23:54:38 -0500

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject distrlib "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :jvm-opt ["-XX:-OmitStackTraceInFastThrow"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/test.check "0.5.9"]
+                 [potemkin "0.3.11"]])
